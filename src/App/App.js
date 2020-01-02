6be354804f6f21f155755bbca28afa98f78c9b3d
Charity Bunyon <cseymou5@gmail.com>
2020-01-01 21:26:05 -0600

    and we have players ladies and gentlemen

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import firebase from 'firebase/app';
-
 import firebaseConnection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
 import Navbar from '../components/Navbar/Navbar';
+import PlayerContainer from '../components/PlayerContainer/PlayerContainer';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 firebaseConnection();
@@ -28,15 +29,22 @@ class App extends React.Component {
     this.removeListener();
   }
 
+  renderView = () => {
+    const { authed } = this.state;
+    if (!authed) {
+      return (<Auth />);
+    }
+    return (<PlayerContainer />);
+  }
 
   render() {
-    const { authed } = this.state; // const authed = this.state.authed
+    const { authed } = this.state;
 
     return (
       <div className="App">
         <Navbar authed={authed} />
         {
-        (authed) ? (<div>You Logged In!</div>) : (<Auth />)
+        this.renderView()
       }
       </div>
       // If they are authenticated load the board
