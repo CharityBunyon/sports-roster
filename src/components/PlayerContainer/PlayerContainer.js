ad4e4e431d74b38fe78b71a32ede67bad4460976
Charity Bunyon <cseymou5@gmail.com>
2020-01-05 16:36:05 -0600

    can add Player

--- src/components/PlayerContainer/PlayerContainer.js
+++ src/components/PlayerContainer/PlayerContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Player from '../Player/Player';
 import authData from '../../helpers/data/authData';
 import playerData from '../../helpers/data/playerData';
+import PlayerForm from '../PlayerForm/PlayerForm';
 
 class PlayerContainer extends React.Component {
   state = {
@@ -28,11 +29,23 @@ class PlayerContainer extends React.Component {
       .catch((errorFromDeleteSinglePlayer) => console.error({ errorFromDeleteSinglePlayer }));
   }
 
+  addPlayer = (newPlayer) => {
+    const uid = authData.getUid();
+    playerData.addNewPlayer(newPlayer)
+      .then(() => {
+        this.getPlayers(uid);
+      })
+      .catch((error) => console.error(error));
+  }
+
   render() {
     return (
-      <div className="d-flex flex-wrap justify-content-between">
-        {this.state.players.map((player) => (<Player key={player.id} player={player} deleteSinglePlayer={this.deleteSinglePlayer} />))}
-      </div>
+    <div>
+      <PlayerForm addPlayer={this.addPlayer} />
+        <div className="d-flex flex-wrap justify-content-between">
+          {this.state.players.map((player) => (<Player key={player.id} player={player} deleteSinglePlayer={this.deleteSinglePlayer} />))}
+        </div>
+    </div>
     );
   }
 }
